d5fdf607d51ea85706be06ef694c98992de670b2
FyFT0000 <facundoft@gmail.com>
2023-09-16 20:37:17 +0200

    2.26_tip-calculator-start.py  Final

--- 2.26_tip-calculator-start.py
+++ 2.26_tip-calculator-start.py
@@ -6,5 +6,9 @@
 
 #Write your code below this line 👇
 
-print(f"Welcome to the tip calculator")
-
+print(f"Welcome to the tip calculator!")
+bill = int(input("What was the total bill? "))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+pay = (bill * (1 + tip / 100)) / people
+print(f'Each person should pay: ${round(pay,1)}')
