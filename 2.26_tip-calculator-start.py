f06a68f97ae0c0d88165566ec67f2d143546489d
FyFT0000 <facundoft@gmail.com>
2023-09-17 17:16:16 +0200

    2.26_tip-calculator-start.py   added roudend by two decimals

--- 2.26_tip-calculator-start.py
+++ 2.26_tip-calculator-start.py
@@ -11,4 +11,4 @@ bill = int(input("What was the total bill? "))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 pay = (bill * (1 + tip / 100)) / people
-print(f'Each person should pay: ${round(pay,1)}')
+print(f'Each person should pay: ${round(pay,2):.2f}')
